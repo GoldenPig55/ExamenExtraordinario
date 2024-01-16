5e1d5b8490f535b4e65a7c56b48a8f38d7ebeb45
Pige <85260656+GoldenPig55@users.noreply.github.com>
2024-01-16 08:11:59 -0600

    implementacion parcial de FindAbility()

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-
-namespace ExamenExtraordinario
+﻿namespace ExamenExtraordinario
 {
     internal class Program
     {
@@ -73,7 +70,97 @@ namespace ExamenExtraordinario
     {
         public void FindAbility()
         {
+            string SeleccionNombre, SeleccionPoA;
+            int SeleccionID;
+            char SeleccionYN;
+            string pasivabuscada = "a", activabuscada = "a";
 
+            Console.WriteLine("Que tipo de abilidad esta buscando? [ 1) Pasiva 2) Activa ]");
+            SeleccionPoA = Console.ReadLine();
+            switch (SeleccionPoA)
+            {
+                case "1":
+                    Console.WriteLine("Ingrese el ID de su habilidad pasiva.");
+                    SeleccionID = Convert.ToInt32(Console.ReadLine());
+                   // var pasivabucada = Program.AbilityRepository._passiveAbilityList.Find(x => x.Id == SeleccionID);
+                    if (pasivabuscada != null)
+                    {
+                        Console.WriteLine(pasivabuscada);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se encontro ninguna habilidad pasiva con ese ID. Desea buscar por nombre? (Y/N)");
+                        SeleccionYN = Convert.ToChar(Console.ReadLine());
+                        switch (SeleccionYN)
+                        {
+                            case 'Y':
+                            case 'y':
+                                Console.WriteLine("Ingrese el nombre de su habilidad pasiva.");
+                                SeleccionNombre = Console.ReadLine();
+                               // pasivabucada = Program.AbilityRepository._passiveAbilityList.Find(x => x.Id == SeleccionID);
+                                if (pasivabuscada != null)
+                                {
+                                    Console.WriteLine(pasivabuscada);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("No se encontro ninguna habilidad pasiva con ese nombre.");
+                                }
+                                break;
+                            case 'N':
+                            case 'n':
+                                break;
+                            default:
+                                Console.WriteLine("Seleccion invalida.");
+                                FindAbility();
+                                break;
+                        }
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("Ingrese el ID de su habilidad activa.");
+                    SeleccionID = Convert.ToInt32(Console.ReadLine());
+                    // var activabuscada = Program.AbilityRepository._activeAbilityList.Find(x => x.Id == SeleccionID);
+                    if (activabuscada != null)
+                    {
+                        Console.WriteLine(activabuscada);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se encontro ninguna habilidad activa con ese ID. Desea buscar por nombre? (Y/N)");
+                        SeleccionYN = Convert.ToChar(Console.ReadLine());
+                        switch (SeleccionYN)
+                        {
+                            case 'Y':
+                            case 'y':
+                                Console.WriteLine("Ingrese el nombre de su habilidad activa.");
+                                SeleccionNombre = Console.ReadLine();
+                               // activabuscada = Program.registro._activeAbilityList.Find(x => x.Nombre == SeleccionNombre);
+                                if (activabuscada != null)
+                                {
+                                    Console.WriteLine(activabuscada);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("No se encontro ninguna mascota con ese nombre.");
+                                }
+                                break;
+                            case 'N':
+                            case 'n':
+                                break;
+                            default:
+                                Console.WriteLine("Seleccion invalida.");
+                                FindAbility();
+                                break;
+                        }
+
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Seleccion invalida.");
+                    FindAbility();
+                    break;
+            }
         }
     }
     public interface IAbility
