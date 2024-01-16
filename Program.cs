043904e9e1e536dcef960db50c97afdf46cfd6bb
Pige <85260656+GoldenPig55@users.noreply.github.com>
2024-01-16 07:10:47 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenExtraordinario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
